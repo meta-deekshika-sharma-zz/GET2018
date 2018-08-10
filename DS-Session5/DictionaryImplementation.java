4625361820f9eb06f6d735995ac70331ab0359f6
meta-deekshika-sharma <deekshika.sharma@metacube.com>
2018-08-10 10:29:28 +0530

    DS-Session5

--- DS-Session5/DictionaryImplementation.java
+++ DS-Session5/DictionaryImplementation.java
@@ -9,175 +9,156 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 /**
- * @author Deekshika Sharma
- * This class is create a dictionary based on key value pair 
+ * @author Deekshika Sharma 
+ * This class is create a dictionary based on key value pair
  */
-public class DictionaryImplementation implements Dictionary{
+public class DictionaryImplementation implements Dictionary {
 
 	private BSTNode root;
 	private List<BSTNode> sortedList;
-	
+
 	public DictionaryImplementation(String initialEntry) {
-		
+
 		JSONParser parser = new JSONParser();
-		 root = null;
+		root = null;
 
-        try {
+		try {
 
-            Object obj = parser.parse(new FileReader(initialEntry));
+			Object obj = parser.parse(new FileReader(initialEntry));
 
-            JSONObject jsonObject = (JSONObject) obj;
-            
-            for(Object obj1: jsonObject.keySet())
-            {
-            	System.out.println(obj1.toString());
-            }
-                
-            
-           
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
+			JSONObject jsonObject = (JSONObject) obj;
+
+			for (Object obj1 : jsonObject.keySet()) {
+				addKeyValuePair(obj1.toString(), jsonObject.get(obj1)
+						.toString());
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-	
+
 	// This method adds the key value pair to dictionary
-	public BSTNode add(BSTNode node, String key, String value)
-	{
+	public BSTNode add(BSTNode node, String key, String value) {
 		if (node == null)
-            node = new BSTNode(key, value);
-        else
-        {
-            if (key.compareTo(node.getKey()) == 0)
-                throw new AssertionError("Duplicate entry");
-            else if (key.compareToIgnoreCase(node.getKey()) < 0)
-            	node.setLeftChild(add(node.getLeftChild(), key, value));
-            else
-                node.setRightChild(add(node.getRightChild(), key, value));
-        }
-        return node;
+			node = new BSTNode(key, value);
+		else {
+			if (key.compareTo(node.getKey()) == 0)
+				throw new AssertionError("Duplicate entry");
+			else if (key.compareToIgnoreCase(node.getKey()) < 0)
+				node.setLeftChild(add(node.getLeftChild(), key, value));
+			else
+				node.setRightChild(add(node.getRightChild(), key, value));
+		}
+		return node;
 	}
-	
+
 	// This method delete the key value pair from dictionary
-	public BSTNode delete(BSTNode node, String key)
-	{
-        BSTNode node1, node2, n;
-        
-        if (node.getKey().equals(key))
-        {
-            BSTNode left, right;
-            left = node.getLeftChild();
-            right = node.getRightChild();
+	public BSTNode delete(BSTNode node, String key) {
+		BSTNode node1, node2, n;
 
-            if (left == null && right == null)
-                return null;
-            else if (left == null)
-            {
-                node1 = right;
-                return node1;
-            }
-            else if (right == null)
-            {
-                node1 = left;
-                return node1;
-            }
-            else
-            {
-                node1 = right;
-                node2 = right;
-                
-                while (node2.getLeftChild() != null)
-                    node2 = node2.getLeftChild();
+		if (node.getKey().equals(key)) {
+			BSTNode left, right;
+			left = node.getLeftChild();
+			right = node.getRightChild();
 
-               node2.setLeftChild(left);
-               return node1;
-            }
-        }
+			if (left == null && right == null)
+				return null;
+			else if (left == null) {
+				node1 = right;
+				return node1;
+			} else if (right == null) {
+				node1 = left;
+				return node1;
+			} else {
+				node1 = right;
+				node2 = right;
 
-        if (key.compareTo(node.getKey()) < 0)
-        {
-            n = delete(node.getLeftChild(), key);
-            node.setLeftChild(n);
-        }
-        else
-        {
-            n = delete(node.getRightChild(), key);
-            node.setRightChild(n);             
-        }
-        
-        return node;
+				while (node2.getLeftChild() != null)
+					node2 = node2.getLeftChild();
+
+				node2.setLeftChild(left);
+				return node1;
+			}
+		}
+
+		if (key.compareToIgnoreCase(node.getKey()) < 0) {
+			n = delete(node.getLeftChild(), key);
+			node.setLeftChild(n);
+		} else {
+			n = delete(node.getRightChild(), key);
+			node.setRightChild(n);
+		}
+
+		return node;
 	}
-	
+
 	// This method is override to add key value pair
 	@Override
 	public BSTNode addKeyValuePair(String key, String value) {
-		root = add(root, key, value);
-		if(root != null)
-		{
-			return root;
+
+		if (isKeyValueValid(key) && isKeyValueValid(value)) {
+			root = add(root, key, value);
+			if (root != null)
+				return root;
 		}
-		else
-			return null;
+		return null;
 	}
 
 	// This method is override to delete key value pair
 	@Override
 	public BSTNode deleteKeyValuePair(String key) {
-		
-		root = delete(root, key);
-		if(root != null)
-		{
-			return root;
+
+		if (isKeyValueValid(key)) {
+			root = delete(root, key);
+			if (root != null)
+				return root;
+
 		}
-		else
-			return null;
+		return null;
 	}
 
 	// This method is find the value for particular key
-	public String getValue(BSTNode node, String key)
-	{
-		String value = "";
-		while(node != null)
-		{
-			if(node.getKey().equals(key))
-				value = node.getKey();
-			else if(node.getKey().compareToIgnoreCase(key) < 0)
+	public String getValue(BSTNode node, String key) {
+		String value = null;
+		if (node != null) {
+			if (node.getKey().equals(key))
+				value = node.getValue();
+			else if (node.getKey().compareToIgnoreCase(key) > 0)
 				value = getValue(node.getLeftChild(), key);
 			else
 				value = getValue(node.getRightChild(), key);
 		}
 		return value;
 	}
-	
+
 	// This method will override to find value for particular key
 	@Override
 	public String getValueForKey(String key) {
-		
-		if(isEmpty())
-			throw new AssertionError("Dictionary is empty");
-		else if(key.length() == 0)
-			throw new AssertionError();
-		else
-			return getValue(root, key);
+
+		if (isKeyValueValid(key)) {
+			if (isEmpty())
+				throw new AssertionError("Dictionary is empty");
+
+		}
+		return getValue(root, key);
+
 	}
 
-	// This method will return the list of nodes in sorted order 
+	// This method will return the list of nodes in sorted order
 	@Override
 	public List<BSTNode> sortedList() {
-			
+
 		sortedList = new ArrayList<BSTNode>();
 		inOrderTraversal(root);
 		return sortedList;
 	}
-	
+
 	// This method will traverse the tree in in order
-	public void inOrderTraversal(BSTNode root)
-	{
-		if(root != null)
-		{
+	public void inOrderTraversal(BSTNode root) {
+		if (root != null) {
 			inOrderTraversal(root.getLeftChild());
 			System.out.print(root.getKey() + " ");
+			sortedList.add(new BSTNode(root.getKey(), root.getValue()));
 			inOrderTraversal(root.getRightChild());
 		}
 	}
@@ -185,37 +166,34 @@ public class DictionaryImplementation implements Dictionary{
 	// This method will return list of keys in between specified key
 	@Override
 	public List<BSTNode> sortedListBetweenKeys(String key1, String key2) {
-	
-        List<BSTNode> sortedSubList = new ArrayList<BSTNode>();
-     
-        Iterator<BSTNode> iterator = sortedList.iterator();    
-        
-        while(iterator.hasNext())
-        {
-            BSTNode entry = iterator.next();
-            if(key1.compareTo(entry.getKey()) >= 0)
-            {
-            	System.out.println(entry.getKey());
-                sortedSubList.add(entry);
-                break;
-            }
-        }
-        
-        while(iterator.hasNext())
-        {
-            BSTNode entry = iterator.next();
-            if(key2.compareTo(entry.getKey()) < 0)
-                break;
-            
-            sortedSubList.add(entry);
-        }
-        inOrderTraversal(root);
-        return sortedSubList;
+
+		List<BSTNode> sortedSubList = new ArrayList<BSTNode>();
+
+		Iterator<BSTNode> iterator = sortedList.iterator();
+
+		while (iterator.hasNext()) {
+			BSTNode entry = iterator.next();
+			if (key1.compareToIgnoreCase(entry.getKey()) < 0
+					&& key2.compareToIgnoreCase(entry.getKey()) > 0)
+				sortedSubList.add(entry);
+			else if (key2.compareToIgnoreCase(entry.getKey()) < 0)
+				break;
+		}
+		for (BSTNode node : sortedSubList)
+			System.out.print(node.getKey() + " ");
+		return sortedSubList;
 	}
 
 	// return true if tree is empty
-	public boolean isEmpty()
-    {
-        return root == null;
-    }
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	// return assertion error if key or value is empty
+	public boolean isKeyValueValid(String string) {
+		if (string.length() == 0)
+			throw new AssertionError("Key or Value is empty");
+		else
+		    return true;
+	}
 }
--- DS-Session5/DictionaryTest.java
+++ DS-Session5/DictionaryTest.java
@@ -2,26 +2,74 @@ package dictionaryImplementation;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Deekshika Sharma
+ * This class will have test cases for Dictionary using binary search tree
+ */
 public class DictionaryTest {
 
-	
-	@Test
-	public void test() {
-		Dictionary dictionaryImplementation = new DictionaryImplementation("D:\\java\\Dictionary\\Trees.json");
+	Dictionary dictionaryImplementation = new DictionaryImplementation("D:\\java\\Dictionary\\Trees.json");
+
+	// This test case is run before all test cases and add key value pair to
+	// dictionary
+	@Before
+	public void addValues() {
+
 		assertNotNull(dictionaryImplementation.addKeyValuePair("d", "dd"));
 		assertNotNull(dictionaryImplementation.addKeyValuePair("b", "bb"));
 		assertNotNull(dictionaryImplementation.addKeyValuePair("c", "cc"));
 		assertNotNull(dictionaryImplementation.addKeyValuePair("a", "aa"));
 		assertNotNull(dictionaryImplementation.addKeyValuePair("e", "ee"));
-		
+		assertNotNull(dictionaryImplementation.addKeyValuePair("g", "gg"));
 		assertNotNull(dictionaryImplementation.sortedList());
-		
-		assertEquals("bb", dictionaryImplementation.getValueForKey("b"));
-		assertNotNull(dictionaryImplementation.deleteKeyValuePair("a"));
-		
-		assertNotNull(dictionaryImplementation.sortedList());
+		assertNotNull(dictionaryImplementation.sortedListBetweenKeys("a", "d"));
 	}
 
+	// This test will delete the key value pair from dictionary
+	@Test
+	public void deleteValues() {
+		assertNotNull(dictionaryImplementation.deleteKeyValuePair("a"));
+		assertNotNull(dictionaryImplementation.deleteKeyValuePair("e"));
+
+		assertNotNull(dictionaryImplementation.sortedList());
+		assertNotNull(dictionaryImplementation.sortedListBetweenKeys("c", "g"));
+	}
+
+	// This test case will return values corresponding to key
+	@Test
+	public void getValues() {
+		assertEquals("bb", dictionaryImplementation.getValueForKey("b"));
+		assertEquals(
+				"Pistachio shells may be helpful in cleaning up pollution created by mercury emissions",
+				dictionaryImplementation.getValueForKey("Pistachio"));
+	}
+
+	// This test case is run for assertion error
+	@Test(expected = AssertionError.class)
+	public void dictionaryErrors() {
+		assertEquals("bb", dictionaryImplementation.getValueForKey("po")); // give
+																			// error
+																			// when
+																			// key
+																			// is
+																			// not
+																			// found
+		assertNotNull(dictionaryImplementation.addKeyValuePair("g", "hh")); // give
+																			// error
+																			// when
+																			// key
+																			// is
+																			// reentered
+		assertNotNull(dictionaryImplementation.addKeyValuePair("", "")); // give
+																			// error
+																			// if
+																			// key
+																			// or
+																			// value
+																			// is
+																			// empty
+	}
 }
