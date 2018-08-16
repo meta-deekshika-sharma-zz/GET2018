0c59962cbda1e6c8af5dbc695db131326ed68043
meta-deekshika-sharma <deekshika.sharma@metacube.com>
2018-08-16 18:50:15 +0530

    DBMS-Session2

---
+++ DBMS-Session2/Assignment1/StoreFrontDescibe.sql
@@ -0,0 +1,12 @@
+use store_front;
+
+describe User;
+describe Role;
+describe UserRoleRelation;
+describe Address;
+describe Orders;
+describe Product;
+describe OrderProductRelation;
+describe Category;
+describe ProductCategoryRelation;
+describe Image;
