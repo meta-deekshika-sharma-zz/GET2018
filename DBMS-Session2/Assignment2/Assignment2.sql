a04c5d041598021d7c7d5d37baa2a1788a6486d5
meta-deekshika-sharma <deekshika.sharma@metacube.com>
2018-08-17 16:53:08 +0530

    DBMS-Session2

---
+++ DBMS-Session2/Assignment2/Assignment2.sql
@@ -0,0 +1,35 @@
+#2
+SELECT p.Id, p.Name, c.Name, p.Price
+FROM Product AS p, Category AS C, ProductCategoryRelation AS cpr
+WHERE p.Id = cpr.ProductId AND c.Id = cpr.CategoryId
+ORDER BY ProductId DESC;
+
+#3
+SELECT p.Id, p.Name, p.Description, p.Price
+FROM Product AS p
+WHERE p.Id IN (SELECT i.ProductId FROM Image as i);
+
+#4
+SELECT c.Id, c.Name, ca.Name
+FROM Category AS c, Category AS ca
+WHERE c.ParentCategoryId = ca.Id
+ORDER BY ca.name, c.name;
+
+#5
+SELECT c.Id, c.Name, ca.Name
+FROM Category AS c, Category AS ca
+WHERE c.ParentCategoryId = ca.Id AND c.Id NOT IN (SELECT ParentCategoryId FROM Category);
+
+#6
+SELECT p.Name, p.Description, p.Price
+FROM Product AS p, Category AS c, ProductCategoryRelation AS cpr
+WHERE c.Name = "Mobiles" AND p.Id = cpr.ProductId AND c.Id = cpr.CategoryId;
+
+#7
+SELECT Name
+FROM Product
+WHERE Stock < 50;
+
+#8
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Product SET Stock = Stock + 100;
---
+++ DBMS-Session2/Assignment3/Assignment3.sql
@@ -0,0 +1,49 @@
+use store_front;
+
+#1
+SELECT o.Id, o.PlacedDate, SUM(p.Price * opr.Quantity)
+FROM Orders AS o, Product AS p, OrderProductRelation AS opr
+WHERE o.Id = opr.OrderId AND p.Id = opr.ProductId
+GROUP BY o.Id
+ORDER BY o.PlacedDate DESC
+LIMIT 50;
+
+#2
+SELECT o.Id, o.PlacedDate, SUM(p.Price * opr.Quantity) AS TotalBill
+FROM Orders AS o, Product AS p, OrderProductRelation AS opr
+WHERE o.Id = opr.OrderId AND p.Id = opr.ProductId
+GROUP BY o.Id
+ORDER BY TotalBill DESC; 
+
+#3
+SELECT opr.OrderId, opr.ProductId
+FROM OrderProductRelation AS opr, Orders As o
+WHERE opr.OrderId = o.Id AND opr.Status != "Shipped" AND DATEDIFF(CURDATE(), o.PlacedDate) > 10;
+
+#4
+SELECT u.Id
+FROM User AS u, Orders As o
+WHERE DATEDIFF(CURDATE(), o.PlacedDate) > 30 AND u.Id = o.UserId;
+
+#5
+SELECT u.Id, o.Id
+FROM User AS u, Orders As o
+WHERE DATEDIFF(CURDATE(), o.PlacedDate) < 15 AND u.Id = o.UserId;
+
+#6
+SELECT opr.ProductId, p.Name
+FROM OrderProductRelation AS opr, Orders AS o, Product AS p
+WHERE opr.Status = "Shipped" AND o.Id = "1" AND p.Id = opr.ProductId;
+
+#7
+SELECT o.PlacedDate, opr.ProductId, o.Id, p.Name
+FROM OrderProductRelation AS opr, Orders AS o, Product AS p
+WHERE p.Price < 50 AND p.Price > 20 AND p.Id = opr.ProductId AND o.Id = opr.OrderId
+GROUP BY o.Id;
+
+#8
+SET SQL_SAFE_UPDATES = 0;
+UPDATE OrderProductRelation
+SET Status = "Shipped"
+WHERE OrderId IN (SELECT Id FROM Orders WHERE PlacedDate = CURDATE())
+LIMIT 20;
