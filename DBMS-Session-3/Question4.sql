221d9031c986ffc3e02857681497ac4159cddd06
meta-deekshika-sharma <41181766+meta-deekshika-sharma@users.noreply.github.com>
2018-08-20 11:02:00 +0530

    Update Question4.sql

--- DBMS-Session-3/Question4.sql
+++ DBMS-Session-3/Question4.sql
@@ -6,12 +6,11 @@ pin_code INT PRIMARY KEY,
 city VARCHAR(25) NOT NULL ,
 state VARCHAR(25) NOT NULL 
 );
-
-INSERT INTO zipcode(pin_code,city,state)
+ INSERT INTO zipcode(pin_code,city,state)
 VALUES(301001,"Alwar","Rajasthan"),
       (302001,"Jaipur","Rajasthan"),
       (400099,"Mumbai","Maharastra"),
-      (38001, "Ahemdabad", "Gujrat");
+      (380001, "Ahemdabad", "Gujrat");
       
 SELECT pin_code , city , state
 FROM Zipcode
