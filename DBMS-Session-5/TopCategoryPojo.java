bb648d23e5662b15ef8b6c3eddc510d5acbae6ba
meta-deekshika-sharma <deekshika.sharma@metacube.com>
2018-08-22 13:01:39 +0530

    DBMS-Session-5

---
+++ DBMS-Session-5/TopCategoryPojo.java
@@ -0,0 +1,39 @@
+package storeFront;
+
+/**
+ * @author Deekshika Sharma
+ * This pojo have Category properties for Category table in store front
+ */
+public class TopCategoryPojo {
+
+	private String parentCategoryName;
+    private int childCategories;
+    
+    public TopCategoryPojo(String parentCategoryName, int childCategories) {
+        
+        this.parentCategoryName = parentCategoryName;
+        this.childCategories = childCategories;
+    }
+
+    public String getParentCategoryName() {
+        return parentCategoryName;
+    }
+    
+    public void setParentCategoryName(String parentCategoryName) {
+        this.parentCategoryName = parentCategoryName;
+    }
+    
+    public int getChildCategories() {
+        return childCategories;
+    }
+    
+    public void setChildCategories(int childCategories) {
+        this.childCategories = childCategories;
+    }
+
+    @Override
+    public String toString() {
+        return "TopCategory [parentCategoryName=" + parentCategoryName
+                + ", childCategories=" + childCategories + "]";
+    }
+}
