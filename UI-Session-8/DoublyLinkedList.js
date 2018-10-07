e83a7ce874339a67b7f85234e16a40a3e41a4288
meta-deekshika-sharma <41181766+meta-deekshika-sharma@users.noreply.github.com>
2018-10-07 23:10:29 +0530

    Update DoublyLinkedList.js

--- UI-Session-8/DoublyLinkedList.js
+++ UI-Session-8/DoublyLinkedList.js
@@ -1,87 +1,80 @@
 //This function defines the node of linked list
 function Node(data) {
-    this.data = data;
-    this.next = null;
+	this.data = data;
+	this.next = null;
 	this.previous = null;
 }
-
 //This function defines a doubly linked list object
 function DoublyList() {
-    this.size = 0;
-    this.head = null;
+	this.size = 0;
+	this.head = null;
 	this.tail = null;
 }
-
 //This add function add value at the starting of list
 DoublyList.prototype.add = function(value) {
-    var node = new Node(value);
- 
-    if (this.head) {
-        this.tail.next = node;
+	var node = new Node(value);
+	
+	if (this.head) {
+		this.tail.next = node;
 		node.previous = this.tail;
 		this.tail = node;
-    } else {
+	else {
 		this.head = node;
 		this.tail = node;
 	}
-	
-    this.size++; 
-    return node;
+	this.size++; 
+        return node;
 };
-
 //This function search node at a specified position
 DoublyList.prototype.searchNodeAt = function(position) {
-    var currentNode = this.head,
-    size = this.size,
-    count = 1,
-    message = {failure: 'Failure: non-existent node in this list.'};
+	var currentNode = this.head,
+	    size = this.size,
+	    count = 1,
+	    message = {failure: 'Failure: non-existent node in this list.'};
  
-    if (size == 0 || position < 1 || position > size) {
-        throw new Error(message.failure);
-    }
+	if (size == 0 || position < 1 || position > size) {
+		throw new Error(message.failure);
+	}
  
-    while (count < position) {
-        currentNode = currentNode.next;
-        count++;
-    }
-    console.log(currentNode.data);
-    return currentNode;
+	while (count < position) {
+		currentNode = currentNode.next;
+        	count++;
+	}
+	console.log(currentNode.data);
+	return currentNode;
 };
-
 //This function will print the linked list data
 DoublyList.prototype.printList = function() {
 	var currentNode = this.head; 
-    var str = ""; 
-    while (currentNode) { 
-        str += currentNode.data + " "; 
-        currentNode = currentNode.next; 
-    } 
-    console.log(str);
+	var str = ""; 
+	while (currentNode) { 
+		str += currentNode.data + " "; 
+		currentNode = currentNode.next; 
+	} 
+	console.log(str);
 }
-
 //This function will remove the element at the specified position
 DoublyList.prototype.remove = function(position) {
-    var currentNode = this.head,
-    size = this.size,
-    count = 1,
-    message = {failure: 'Failure: non-existent node in this list.'},
-    beforeNodeToDelete = null,
-	afterNodeToDelete = null,
-    nodeToDelete = null;
+	var currentNode = this.head,
+	    size = this.size,
+	    count = 1,
+	    message = {failure: 'Failure: non-existent node in this list.'},
+	    beforeNodeToDelete = null,
+	    afterNodeToDelete = null,
+	    nodeToDelete = null;
  
-    if (size === 0 || position < 0 || position > size) {
-        throw new Error(message.failure);
-    }
-
-    if (position === 1) {
-        this.head = currentNode.next;
-		
+	if (size === 0 || position < 0 || position > size) {
+		throw new Error(message.failure);
+	} else {
+        
+		if (position === 1) {
+			this.head = currentNode.next;
 		if(!this.head) {
 			this.tail = null;
 		} else {
 			this.head.previous = null;
 		}
-    } else if (position == this.size) {
+	} else if (position == this.size) {
 		this.tail = this.tail.previous;
 		this.tail.next = null;
 	} else {
@@ -89,8 +82,7 @@ DoublyList.prototype.remove = function(position) {
 			currentNode = currentNode.next;
 			count++;
 		}
-		
-		beforeNodeToDelete = currentNode.previous;
+        	beforeNodeToDelete = currentNode.previous;
 		afterNodeToDelete = currentNode.next;
 		
 		beforeNodeToDelete.next = afterNodeToDelete;
@@ -98,6 +90,8 @@ DoublyList.prototype.remove = function(position) {
 		nodeToDelete = currentNode;
 		currentNode = null;
 	}
+	this.size--;
+    }
 };
 
 var doublyList = new DoublyList();
