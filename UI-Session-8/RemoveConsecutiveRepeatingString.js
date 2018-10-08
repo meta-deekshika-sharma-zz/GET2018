6c60ea796dd0a5f0185537b3bb37ee4d1cc5ef6b
meta-deekshika-sharma <41181766+meta-deekshika-sharma@users.noreply.github.com>
2018-10-08 10:56:56 +0530

    Update RemoveConsecutiveRepeatingString.js

--- UI-Session-8/RemoveConsecutiveRepeatingString.js
+++ UI-Session-8/RemoveConsecutiveRepeatingString.js
@@ -1,19 +1,18 @@
+
 function removeConsecutiveRepeatingString(inputString) {
-   var i = 0;
+   var count=0;
    
-   while(i < inputString.length - 1) {
-       var n = 0;
-       var j = i;
-       while(inputString.charAt(j) == inputString.charAt(j + 1)) {
-           n++;
-           j++;
-       }
-       if(n > 0) {
-           inputString = inputString.substring(0, i) + inputString.substring(i + n + 1, inputString.length);
-           console.log(inputString);
-           i = i - n - 1;
-       }
-       i++;
+   for (var index = 0; index < inputString.length-1; index++) {
+      if (inputString[index]===inputString[index + 1]) {
+         count++;
+      } else {
+         if (count>0) {
+            inputString = inputString.substring(0,index-count).concat(inputString.substring(index+1,inputString.length));
+            index=0;
+         }
+         count=0;
+      }
    }
+   console.log(inputString);
    return inputString;
 }
