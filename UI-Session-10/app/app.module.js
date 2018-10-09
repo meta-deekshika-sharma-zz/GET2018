a2da3d16f626b43cda458696e2b2eefb03b73a7e
meta-deekshika-sharma <deekshika.sharma@metacube.com>
2018-10-09 19:24:10 +0530

    UI-Session-10

--- UI-Session-10/app/app.module.js
+++ UI-Session-10/app/app.module.js
@@ -1,6 +1,5 @@
 'use strict';
 
 angular.module('userApp', [
-   /* 'phoneDetail',*/
     'userList'
 ]);
--- UI-Session-10/app/user-list/user-list.component.js
+++ UI-Session-10/app/user-list/user-list.component.js
@@ -1,15 +1,45 @@
-'use strict';
-
 angular.
   module('userList').
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
-    controller: ['$http', function UserListController($http) {
+    controller: ['$http', '$scope', function UserListController($http, $scope) {
       var self = this;
-      self.orderProp = 'name';
+      
+      $scope.createUser = function() {
+        $scope.header = "Add";
+        self.user = {
+          picture: "images//male-user-shadow_318-34042.jpg",
+          name: null,
+          phone: null,
+          address: null
+        }  
+      }
+      
+      $scope.editUser = function(userIndex) {
+         $scope.header = "Edit";
+          
+         if(userIndex != null) {
+                self.user.name = self.users[userIndex].name;
+                self.user.phone = self.users[userIndex].phone;
+                self.user.address = self.users[userIndex].address;
+                self.user.userIndex = userIndex;
+          }
+      }
 
       $http.get('users/users.json').then(function(response) {
         self.users = response.data;
       });
+        
+      $scope.addUser = function() {
+          
+          if(self.user.userIndex != null)
+                self.users[self.user.userIndex] = self.user;
+            else if(self.user.name != null && self.user.phone != null && self.user.address != null)
+                self.users.push(angular.copy(self.user));
+      }
+       
+      $scope.searchUser = function() {
+        self.query = document.getElementById("searchKeyword").value;
+      }
     }]
   });
