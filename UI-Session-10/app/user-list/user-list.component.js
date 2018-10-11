fe41513182478e01a7572b779264f26a10b39c95
meta-deekshika-sharma <41181766+meta-deekshika-sharma@users.noreply.github.com>
2018-10-11 12:52:42 +0530

    Update user-list.component.js

--- UI-Session-10/app/user-list/user-list.component.js
+++ UI-Session-10/app/user-list/user-list.component.js
@@ -1,10 +1,12 @@
+//The component part will executes all the functionalities of add, edit and displaying user data
+
 angular.
   module('userList').
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
     controller: ['$http', '$scope', function UserListController($http, $scope) {
       var self = this;
-      
+       
       $scope.createUser = function() {
         $scope.header = "Add";
         self.user = {
@@ -17,13 +19,13 @@ angular.
       
       $scope.editUser = function(userIndex) {
          $scope.header = "Edit";
-          
+        
          if(userIndex != null) {
                 self.user.name = self.users[userIndex].name;
                 self.user.phone = self.users[userIndex].phone;
                 self.user.address = self.users[userIndex].address;
                 self.user.userIndex = userIndex;
-          }
+          } 
       }
 
       $http.get('users/users.json').then(function(response) {
@@ -31,11 +33,17 @@ angular.
       });
         
       $scope.addUser = function() {
-          
           if(self.user.userIndex != null)
                 self.users[self.user.userIndex] = self.user;
-            else if(self.user.name != null && self.user.phone != null && self.user.address != null)
-                self.users.push(angular.copy(self.user));
+          else if(self.user.name != null && self.user.phone != null && self.user.address != null)
+                self.users.push(self.user);
+          
+          self.user = {
+          picture: "images//male-user-shadow_318-34042.jpg",
+          name: null,
+          phone: null,
+          address: null
+        }  
       }
        
       $scope.searchUser = function() {
