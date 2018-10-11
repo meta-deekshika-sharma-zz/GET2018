1ee64979ab616ae49785cbbb720c43096b2a359b
meta-deekshika-sharma <41181766+meta-deekshika-sharma@users.noreply.github.com>
2018-10-11 12:58:59 +0530

    Update app.config.js

--- UI-Session-10/app/app.config.js
+++ UI-Session-10/app/app.config.js
@@ -1,17 +1,4 @@
 'use strict';
 
 angular.
-  module('userApp').
-  config(['$locationProvider' ,'$routeProvider',
-    function config($locationProvider, $routeProvider) {
-
-      $routeProvider.
-        when('/users', {
-          template: '<user-list></user-list>'
-        }).
-       /* when('/phones/:phoneId', {
-          template: '<phone-detail></phone-detail>'
-        }).*/
-        otherwise('/users');
-    }
-  ]);
+  module('userApp', [userList]);
